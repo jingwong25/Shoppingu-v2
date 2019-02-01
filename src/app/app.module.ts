f7ad7fdfd02fbac2563fa4c1582922e93dfc7706
Pong <jingwong25@gmail.com>
2019-02-01 20:43:45 +0800

    Revamp frontned
    Revamp frontned

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profiles/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, ProfileComponent]
+})
+export class AppModule { }
